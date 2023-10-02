be57dd868673e85a3078d808c6f72155ac9195a4
SardorNigmatov <sardornigmatov2003@gmail.com>
2023-10-02 18:52:46 +0500

    share and send email add

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostView, post_detail
+from .views import PostView, post_detail, post_share
 
 app_name = 'blog'
 
 urlpatterns = [
     path('',PostView.as_view(),name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:slug>/',post_detail,name='post_detail')
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>/',post_detail,name='post_detail'),
+    path('<int:post_id>/share/',post_share,name='post_share')
 ]
--- config/settings.py
+++ config/settings.py
@@ -135,3 +135,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'sardordeveloper03@gmail.com'
+EMAIL_HOST_PASSWORD = "password"
+EMAIL_PORT = 465  #587
+EMAIL_USE_TLS = True
